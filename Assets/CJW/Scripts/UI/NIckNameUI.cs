f4164701ea6eeacdf7b7d8d3cc063eaf5514c423
OneJi <2004ashley@naver.com>
2026-05-09 16:46:43 +0900

    [Fix] S1 상호작용 UI 및 닉네임 레이어 수정

--- Assets/CJW/Scripts/UI/NIckNameUI.cs
+++ Assets/CJW/Scripts/UI/NIckNameUI.cs
@@ -5,6 +5,20 @@ public class NicknameUI : MonoBehaviour
 {
     [SerializeField] private TMP_Text nicknameText;
 
+    private void Awake()
+    {
+        if (nicknameText == null)
+            nicknameText = GetComponent<TMP_Text>();
+
+        MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+
+        if (meshRenderer != null)
+        {
+            meshRenderer.sortingLayerName = "Default";
+            meshRenderer.sortingOrder = 200;
+        }
+    }
+
     private void OnEnable()
     {
         DataManager.OnProfileChanged += RefreshNickname;
