47b76f00e25829593436837f59fc246a3ff280ef
h27480suhyeon <h27480suhyeon@naver.com>
2026-05-13 03:34:17 +0900

    [Add] 미니맵 추가 , location 표시

--- Assets/PSH/Scripts/PublicUIManager.cs
+++ Assets/PSH/Scripts/PublicUIManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PublicUIManager : MonoBehaviour
 {
@@ -35,12 +36,8 @@ public class PublicUIManager : MonoBehaviour
 
     public void ExitGame()
     {
-        #if UNITY_EDITOR
-        // 유니티 에디터에서 플레이 모드 종료
-        EditorApplication.isPlaying = false;
-        #else
-        // 실제 빌드된 앱 종료
-        Application.Quit();
-        #endif
+
+        SceneManager.LoadScene("S0_Lobby");
     }
+
 }
